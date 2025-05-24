31bb371dc96966a3563d9df22de4d9e24487dba0
JoaquinRouge <Joarouge@gmail.com>
2025-05-24 18:38:34 -0300

    api-gateway service added

---
+++ api-gateway/src/test/java/com/joaquinrouge/clinics/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquinrouge.clinics.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
